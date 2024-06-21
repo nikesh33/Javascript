cb943c8611411c36e560edd0407bd601c92d3ec9
Nikesh Vishwakarma <nikeshvishwakarma33@gmail.com>
2024-06-21 21:37:50 +0530

    added variables file

---
+++ 02_variables/test.js
@@ -0,0 +1,15 @@
+/* Variable in Js 
+==>A Named Container for storing values like
+==>Number,Strings,Objects or Arrays that can be reassigned and used throught your code*/
+const customerId = 12345;
+let customerEmail = "Nikeshvish@gmail.com";
+var password = "2211";
+userName = "Nikesh";
+let number;
+
+// customerId = 1245 //not allowed
+customerEmail = "vishalv@gmail.com";
+password = 1234567;
+userName = "Vishal";
+
+console.table([customerId, customerEmail, password, userName, number]);
