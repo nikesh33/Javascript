e5e01504656edd794beb8f0d5a2681bd41d1ac78
Nikesh Vishwakarma <nikeshvishwakarma33@gmail.com>
2024-06-21 22:49:45 +0530

    variable and data type conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "1";
+
+// checking type of datatype
+console.log(typeof score);
+console.log(typeof score);
+
+// conversion from string to number
+let inputScore = Number(score);
+console.log(inputScore);
+
+// this is  number
+let isLoggedIn = 1;
+
+// this is an conversion of bool where 1 will be converted to true or false
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//  1 => true; 0 => false
+// ""  => false
+// "Nikesh" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+// data type
+console.log(typeof stringNumber);
+// value
+console.log(stringNumber);
