a4bbbf83ade5c64264724084f9d40cc6882cd0b1
Nikesh Vishwakarma <nikeshvishwakarma33@gmail.com>
2024-06-22 16:09:14 +0530

    dates

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,12 @@
+// /* Dates */
+
+// let myDate = new Date();
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+// custom data
+let myCreatedDate = new Date(2024, 1, 24)
+console.log(myCreatedDate.toDateString())
